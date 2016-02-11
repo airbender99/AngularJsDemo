bb18157e2eb93f143997a5f358cf5ec72e5b3f0e
airbender99 <bender.air99@gmail.com>
2016-02-11 10:24:56 +0530

    Create OrdersController.js

---
+++ customerApp/app/controllers/OrdersController.js
@@ -0,0 +1,26 @@
+//orders controller
+(function(){
+    'use strict'// does not allow to create variables without var keyword to avoid creating global variables.
+    //test = 'Hello';
+    var OrdersController = function($scope, $routeParams, customersFactory){
+        var customerId = $routeParams.customerId;
+        //$scope.orders = null;
+        $scope.customer = null;
+        
+        function init(){
+            //$scope.customers = customersFactory.getCustomers();
+            //for(var i=0, len = $scope.customers.length; i<len ;i++){
+            //    if($scope.customers[i].id === parseInt(customerId)){
+            //        $scope.orders = $scope.customers[i].orders;
+            //        break;
+            //    }
+            //}
+            $scope.customer = customersFactory.getCustomer(customerId);
+        }
+        
+        init();
+    }
+    
+    OrdersController.$inject = ['$scope','$routeParams','customersFactory'];
+    angular.module('customersApp').controller('OrdersController',OrdersController);
+}());
